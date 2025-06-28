177d1d326660a934259432feef3f295d6b760039
rubatotree <zhuyutian@mail.ustc.edu.cn>
2025-06-28 22:27:43 +0800

    视觉：简单修改了相机使得在边缘处不生硬

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	MusicPlayer.play_bgm()
+	# MusicPlayer.play_bgm()
+	pass
--- scripts/room/my_camera.gd
+++ scripts/room/my_camera.gd
@@ -4,9 +4,17 @@ extends Camera2D
 
 ## 相机跟随的平滑度。数值越小，相机移动越“滞后”；数值越大，跟随越紧密。
 @export var smoothing_speed: float = 5.0
+@export var room_width: float = 2880.0
+
+var window_width
+
+func _ready() -> void:
+	window_width = get_viewport().get_visible_rect().size.x
 
 func _physics_process(delta: float) -> void:
 	if not follow_character:
 		push_error("Camera's follower is not setted!")
 		return
-	global_position = lerp(global_position, follow_character.global_position, smoothing_speed * delta)
+	var target_position = follow_character.global_position
+	target_position.x = clamp(target_position.x, window_width / 2, room_width - window_width / 2)
+	global_position = lerp(global_position, target_position, smoothing_speed * delta)
